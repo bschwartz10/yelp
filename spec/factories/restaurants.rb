da913f2d856439cf90bf87f02442ecc9592b4e90
bschwartz10 <bschwartz92@gmail.com>
2017-07-15 21:20:53 -0600

    restaurant factory

---
+++ spec/factories/restaurants.rb
@@ -0,0 +1,10 @@
+# FactoryGirl.define do
+#   factory :restaurant do
+#     name "Carlos Bakery"
+#     cuisine "Pastries"
+#     city "Hoboken"
+#     state "NJ"
+#     zip "07030"
+#     user
+#   end
+# end
