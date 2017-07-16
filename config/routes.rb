8d45da53fb36f393f605c53a9b941c9829a0d34c
bschwartz10 <bschwartz92@gmail.com>
2017-07-15 21:20:10 -0600

    add logout and post restaurants routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'dashboard#index'
   resources :users, only: [:new, :create]
-
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
+  resources :restaurants, only: [:create]
 end
