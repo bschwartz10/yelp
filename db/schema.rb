c87a1b00d9c06140c63688dcd579e02c2318d269
bschwartz10 <bschwartz92@gmail.com>
2017-07-15 21:20:27 -0600

    add restaurants table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170713202743) do
+ActiveRecord::Schema.define(version: 20170715222029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "cuisine"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_restaurants_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -22,4 +34,5 @@ ActiveRecord::Schema.define(version: 20170713202743) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "restaurants", "users"
 end
